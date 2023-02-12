bb8e9b8867bc30c5112d4d4cae69f0f720f74b9d
masasao <ninjinuragosi@gmail.com>
2023-02-12 14:35:31 +0900

    x fix

--- test.bash
+++ test.bash
@@ -2,6 +2,25 @@
 # SPDX-FileCopyrightText: 2023 masasao　　　　　
 # SPDX-License-Identifier: BSD-3-Clause
 
-out=$(./x 1 2 3 4 5)
+ng () {
+	        echo NG at Line $1
+	       	res=1
+}
+res=0
++ res=0
 
-[ "${out}" = 120.0 ]
+
+out=$(./x 1 2 3 4 5)
+[ "${out}" = 120 ] || ng ${LINENO}
+
+out=$(./x あ )
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(./x echo )
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
+[ "$res" = 0 ] && echo OK
+exit $res
